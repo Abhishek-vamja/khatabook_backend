b4316003137f6bb5da01bf495c3f0b741ce2ac8c
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-11-30 15:32:42 +0530

    Add profile images

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Admin for User model."""
+    list_display = ("email", "name", "is_staff", "is_active")
+    search_fields = ("email", "name")
+    readonly_fields = ("created_at", "updated_at")
--- apps/user/models.py
+++ apps/user/models.py
@@ -36,6 +36,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    imges = models.ImageField(upload_to='user_images/', blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
