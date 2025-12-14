a71511cdfc00417ae83cd1db5659700da070bdd3
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-12-14 19:12:05 +0530

    fix codedeploy directory alignment

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
 cd /home/ec2-user/khatabook_backend
 source venv/bin/activate
 
 pkill gunicorn || true
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py migrate || true
+python manage.py collectstatic --noinput || true
 
-gunicorn khatabook_backend.wsgi:application   --bind 127.0.0.1:8000   --daemon
+gunicorn khatabook_backend.wsgi:application \
+  --bind 127.0.0.1:8000 \
+  --daemon
+
+exit 0
