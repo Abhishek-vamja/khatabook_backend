0c63fb6819bef50320987237c6bb83f6aba10d99
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-12-14 20:32:06 +0530

    Modified admin file

--- apps/transacction/admin.py
+++ apps/transacction/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from apps.transacction.models import Transaction
 
-# @admin.register(Transaction)
-# class TransactionAdmin(admin.ModelAdmin):
-#     """Admin interface for Transaction model."""
-#     list_display = ('id', 'user', 'entry_name', 'amount', 'type', 'created_at')
-#     list_filter = ('type', 'user')
-#     search_fields = ('entry_name', 'user__username', 'user__email')
-#     ordering = ('-created_at',)
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    """Admin interface for Transaction model."""
+    list_display = ('id', 'user', 'entry_name', 'amount', 'type', 'created_at')
+    list_filter = ('type', 'user')
+    search_fields = ('entry_name', 'user__username', 'user__email')
+    ordering = ('-created_at',)
