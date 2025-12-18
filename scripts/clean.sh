a96a579b7b37a73ab1f4350b36d46a5b0e0231a8
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-12-18 22:47:27 +0530

    Fixed

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-set -e
 
 TARGET="/home/ec2-user/khatabook_backend"
 
-echo "Cleaning old deployment..."
-rm -rf $TARGET/*
-rm -rf $TARGET/.[!.]*
-rm -rf $TARGET/..?*
+echo "Running clean step..."
+
+if [ -d "$TARGET" ]; then
+  echo "Cleaning $TARGET"
+  rm -rf "$TARGET"/*
+  rm -rf "$TARGET"/.[!.]* || true
+  rm -rf "$TARGET"/..?* || true
+else
+  echo "Target directory does not exist, skipping clean"
+fi
+
+exit 0
