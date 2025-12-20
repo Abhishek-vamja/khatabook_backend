be2e8e19c5a791499331ec5017bbd5435588c3d1
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-12-20 15:33:18 +0530

    Updated user admin file

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from apps.user.models import User
 
 
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     """Admin for User model."""
-#     list_display = ("email", "name", "is_staff", "is_active")
-#     search_fields = ("email", "name")
-#     readonly_fields = ("created_at", "updated_at")
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Admin for User model."""
+    list_display = ("email", "name", "is_staff", "is_active")
+    search_fields = ("email", "name")
+    readonly_fields = ("created_at", "updated_at")
