d5b52841993a30e571f91777d001a886067fcd45
abhishek-vamja <abhishekvamja2518@gmail.com>
2025-12-20 13:58:19 +0530

    fix CodeDeploy to deploy full repo

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -e
 
 TARGET="/home/ec2-user/khatabook_backend"
 
 echo "Cleaning old code..."
 
 if [ -d "$TARGET" ]; then
-  rm -rf "$TARGET"/*
-  rm -rf "$TARGET"/.[!.]* || true
-  rm -rf "$TARGET"/..?* || true
+  rm -rf "$TARGET"
 fi
 
-exit 0
+mkdir -p "$TARGET"
+chown -R ec2-user:ec2-user "$TARGET"
+
+echo "Cleanup completed."
